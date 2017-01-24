7daaef366554c95c012d770da8d3f0321e09cc54
calmery <g1531046@m.sojo-u.ac.jp>
2017-01-24 15:52:15 +0900

    バナーを追加

---
+++ js/banners.js
@@ -0,0 +1,30 @@
+var bannerBaseUrl = 'img/banners/';
+
+var banners = {
+    
+    'ビットリバー株式会社': {
+        url : 'http://bitriver.jp',
+        src : 'banner_bitriver.png',
+        plan: 'bronze'
+    },
+    
+    '株式会社SQUEEZE': {
+        url : 'https://squeeze-inc.co.jp/',
+        src : 'squeeze-logo-horizontal.png',
+        plan: 'silver'
+    },
+    
+    '株式会社ワイズ・リーディング': {
+        url : 'http://ysreading.co.jp/',
+        src : 'ysreading_logo.png',
+        plan: 'bronze'
+    },
+    
+    // バナーリンク URL が画像のためひとまず公式サイトをリンクとしておく
+    '株式会社XOXZO': {
+        url : 'https://www.xoxzo.com/ja/',
+        src : 'xoxzo_logot02.png',
+        plan: 'bronze'
+    },
+    
+};
